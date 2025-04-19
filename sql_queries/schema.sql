ce93484373e96dbd730bbdbb5cbc95a226b05afb
Dmitry <Dmitrii.95@yandex.ru>
2025-04-19 19:45:19 +0300

    Create schema.sql

---
+++ sql_queries/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+  id INT PRIMARY KEY,
+  name VARCHAR(50),
+  email VARCHAR(100)
+);
+
+INSERT INTO users VALUES 
+  (1, 'Иван', 'ivan@test.com'),
+  (2, 'Мария', 'maria@gmail.com');
+  (3, 'Антон', 'anton@gmail.com'),
+  (4, 'Дима', 'dima@test.com'),
+  (5, 'Алексей', 'alexey@gmail.com');
